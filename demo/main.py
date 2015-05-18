fb8d0520d94ba4ed45d74c187ed1d4414f64728c
josuekuri <josuek@hotmail.com>
2015-05-17 23:30:58 -0700

    Corrected stock split in AAPL and GOOGL time series

--- demo/main.py
+++ demo/main.py
@@ -16,8 +16,17 @@ if __name__ == '__main__':
     start = datetime.datetime(2011, 4, 20)
     end = datetime.datetime(2015, 5, 16)
     TS['TS.YHOO'] = list(web.DataReader("YHOO", 'yahoo', start, end)['Close'].values)
-    TS['TS.GOOGL'] = list(web.DataReader("GOOGL", 'yahoo', start, end)['Close'].values)
-    TS['TS.AAPL'] = list(web.DataReader("AAPL", 'yahoo', start, end)['Close'].values)
+
+    # Correcting GOOGL time series for stock split
+    L = web.DataReader("GOOGL", 'yahoo', start, end)
+    L.loc[:'2014-04-02','Close'] = L[:'2014-04-02']['Close']/2
+    TS['TS.GOOGL'] = list(L['Close'].values)
+
+    # Correcting AAPL time series for stock split
+    L = web.DataReader("AAPL", 'yahoo', start, end)
+    L.loc[:'2014-06-06','Close'] = L[:'2014-06-06']['Close']/7
+    TS['TS.AAPL'] = list(L['Close'].values)
+
     TS['TS.MSFT'] = list(web.DataReader("MSFT", 'yahoo', start, end)['Close'].values)
     # Each entry TSwfv[TSname] is an array of vector coefficients
     # Each item TSwfv[TSname][i] in the array is a feature vector (FV)
