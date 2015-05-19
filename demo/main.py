749f8c63f7a2cb7c9c56c3bcfef4f9123a2cbdf0
josuekuri <josuek@hotmail.com>
2015-05-18 18:56:36 -0700

    Added more stocks

--- demo/main.py
+++ demo/main.py
@@ -6,27 +6,60 @@ import wfvg
 if __name__ == '__main__':
     TS = {}
     TSpct = {}
+    symbols = ['YHOO',
+    'GOOGL',
+    'AAPL',
+    'MSFT',
+    'BIDU',
+    'IBM',
+    'EBAY',
+    'ORCL',
+    'CSCO',
+    'SAP',
+    'VZ',
+    'T',
+    'CMCSA',
+    'AMX',
+    'QCOM',
+    'NOK',
+    'AMZN',
+    'WMT',
+    'COST',
+    'TGT',
+    'CVX',
+    'TOT',
+    'BP',
+    'XOM',
+    'E',
+    'COP',
+    'APA',
+    'GS',
+    'MS',
+    'BK',
+    'CS',
+    'SMFG',
+    'DB',
+    'RY',
+    'CS',
+    'BCS',
+    'SAN',
+    'BNPQY']
+    stock_split = {}
+    stock_split['GOOGL'] = ('2014-04-02',2)
+    stock_split['AAPL'] = ('2014-06-06',7)
+    # TO DO: Check splits for AMX, COP, NOK 
 
     start = datetime.datetime(2011, 4, 20)
     end = datetime.datetime(2015, 5, 16)
-    # Retrieve YHOO
-    TS['TS.YHOO'] = list(web.DataReader("YHOO", 'yahoo', start, end)['Close'].values)
 
-    # Retrieve GOOGL
-    L = web.DataReader("GOOGL", 'yahoo', start, end)
-    # Correcting for stock split
-    L.loc[:'2014-04-02','Close'] = L[:'2014-04-02']['Close']/2
-    TS['TS.GOOGL'] = list(L['Close'].values)
-
-    # Retrieve AAPL
-    L = web.DataReader("AAPL", 'yahoo', start, end)
-    # Correcting for stock split
-    L.loc[:'2014-06-06','Close'] = L[:'2014-06-06']['Close']/7
-    TS['TS.AAPL'] = list(L['Close'].values)
-
-    # Retrieve MSFT
-    TS['TS.MSFT'] = list(web.DataReader("MSFT", 'yahoo', start, end)['Close'].values)
-
+    for ticker in symbols:
+        key = "TS."+ticker
+        print "Retrieving {0} ...".format(ticker)
+        L = web.DataReader(ticker, 'yahoo', start, end)
+        # Correct stock splits
+        if ticker in stock_split:
+            L.loc[:stock_split[ticker][0],'Close'] = L[:stock_split[ticker][0]]['Close']/stock_split[ticker][1]
+        TS[key] = list(L['Close'].values)
 
     # Generate normalized time series
     for TSname in TS:
