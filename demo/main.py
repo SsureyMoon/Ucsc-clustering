dcd3c8c0bb06165c551da041bbcbe177b624bf6f
josuekuri <josuek@hotmail.com>
2015-05-19 00:01:42 -0700

    Logic for clustering with K-means

--- demo/main.py
+++ demo/main.py
@@ -2,52 +2,22 @@ import random
 import pandas.io.data as web
 import datetime
 import wfvg
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 if __name__ == '__main__':
     TS = {}
     TSpct = {}
-    symbols = ['YHOO',
-    'GOOGL',
-    'AAPL',
-    'MSFT',
-    'BIDU',
-    'IBM',
-    'EBAY',
-    'ORCL',
-    'CSCO',
-    'SAP',
-    'VZ',
-    'T',
-    'CMCSA',
-    'AMX',
-    'QCOM',
-    'NOK',
-    'AMZN',
-    'WMT',
-    'COST',
-    'TGT',
-    'CVX',
-    'TOT',
-    'BP',
-    'XOM',
-    'E',
-    'COP',
-    'APA',
-    'GS',
-    'MS',
-    'BK',
-    'CS',
-    'SMFG',
-    'DB',
-    'RY',
-    'CS',
-    'BCS',
-    'SAN',
-    'BNPQY']
+    symbols = ['YHOO','GOOGL','AAPL','MSFT','BIDU','IBM','EBAY','ORCL','CSCO',
+    'SAP','VZ','T','CMCSA','AMX','QCOM','NOK','AMZN','WMT','COST','TGT','CVX',
+    'TOT','BP','XOM','E','COP','APA','GS','MS','BK','CS','SMFG','DB','RY','CS',
+    'BCS','SAN','BNPQY','NKE','DECK']
     stock_split = {}
     stock_split['GOOGL'] = ('2014-04-02',2)
     stock_split['AAPL'] = ('2014-06-06',7)
     stock_split['AMX'] = ('2011-06-30',2)
+    stock_split['NKE'] = ('2012-12-25',2)
 
     start = datetime.datetime(2011, 4, 20)
     end = datetime.datetime(2015, 5, 16)
@@ -73,5 +43,19 @@ if __name__ == '__main__':
     # Input to the clustering algorithm is all FVs with the same value of i
     # Example: TSwfv['TS.1'][1], TSwfv['TS.2'][1], TSwfv['TS.3'][1], ...
 
-    # TSwfv,TSsse = wfvg.generate_feature_vectors(TS)
-    TSwfv,TSsse = wfvg.generate_feature_vectors(TSpct)
+    TSwfv,TSsse,TSrec = wfvg.generate_feature_vectors(TSpct)
+
+    # Retrieve the L4 coefficients for all time series
+    dataset = [ TSwfv[k][1] for k,v in TSwfv.iteritems() ]
+    datasetnames = [k for k,v in TSwfv.iteritems()]
+    model = KMeans(n_clusters=8).fit(dataset)
+    TScL4 = dict(zip(datasetnames, model.labels_))
+    print TScL4
+    for i in range(min(model.labels_),max(model.labels_)+1):
+        print "Cluster", i
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        for k in [key for key,val in TScL4.iteritems() if TScL4[key]==i]:
+            ax.plot(TSrec[k][1],label=k+' L4 ')
+        plt.legend(prop={'size':6},loc=0)
+        plt.savefig('Cluster'+str(i)+'.pdf',edgecolor='b', format='pdf')
--- demo/wfvg.py
+++ demo/wfvg.py
@@ -7,14 +7,17 @@ wavelettype = 'haar'
 def generate_feature_vectors(TS):
 	TSwfv = {} # Wavelet feature vector
 	TSsse = {} # Sum of Square Errors
+	TSrec = {} # reconstructed time series
 	for TSname in TS:
 		cL2 = pywt.wavedec(TS[TSname],wavelettype,level=2)
 		cL4 = pywt.wavedec(TS[TSname],wavelettype,level=4)
 		cL6 = pywt.wavedec(TS[TSname],wavelettype,level=6)
+		cL8 = pywt.wavedec(TS[TSname],wavelettype,level=8)
 		# Generate array of feature vectors for this time series
 		TSwfv[TSname] = [list(cL2[0])+list(cL2[1]), \
 			list(cL4[0])+list(cL4[1]), \
-			list(cL6[0])+list(cL6[1])]
+			list(cL6[0])+list(cL6[1]), \
+			list(cL8[0])+list(cL8[1])]
 		#print len(TSwfv[TSname][0])
 		#print len(TSwfv[TSname][1])
 		#print len(TSwfv[TSname][2])
@@ -23,39 +26,50 @@ def generate_feature_vectors(TS):
 
 		# Generate array of SSE values for this time series
 		TSsse[TSname] = []
+
 		# Level 2
 		c = [cL2[0],cL2[1]]
 		for i in range(2,len(cL2)):
 			c.append([0]*len(cL2[i]))
-
 		TSrecL2 = pywt.waverec(c,wavelettype)
 		sse = sum([(TS[TSname][i]-TSrecL2[i])**2 for i,v in enumerate(TSrecL2)])
 		TSsse[TSname].append(sse)
+
 		# Level 4
 		c = [cL4[0],cL4[1]]
 		for i in range(2,len(cL4)):
 			c.append([0]*len(cL4[i]))
-
 		TSrecL4 = pywt.waverec(c,wavelettype)
 		sse = sum([(TS[TSname][i]-TSrecL4[i])**2 for i,v in enumerate(TSrecL4)])
 		TSsse[TSname].append(sse)
+
 		# Level 6
 		c = [cL6[0],cL6[1]]
 		for i in range(2,len(cL6)):
 			c.append([0]*len(cL6[i]))
-
 		TSrecL6 = pywt.waverec(c,wavelettype)
 		sse = sum([(TS[TSname][i]-TSrecL6[i])**2 for i,v in enumerate(TSrecL6)])
 		TSsse[TSname].append(sse)
 
-		# Plotting
-		fig = plt.figure()
-		ax = fig.add_subplot(111)
-		ax.plot(TS[TSname],label=TSname)
-		ax.plot(TSrecL2,label=TSname+' (L2) SSE: '+str(TSsse[TSname][0]))
-		ax.plot(TSrecL4,label=TSname+' (L4) SSE: '+str(TSsse[TSname][1]))
-		ax.plot(TSrecL6,label=TSname+' (L6) SSE: '+str(TSsse[TSname][2]))
-		plt.legend(prop={'size':6})
-		plt.savefig(TSname+'.pdf',edgecolor='b', format='pdf')
+		# Level 8
+		c = [cL8[0],cL8[1]]
+		for i in range(2,len(cL8)):
+			c.append([0]*len(cL8[i]))
+		TSrecL8 = pywt.waverec(c,wavelettype)
+		sse = sum([(TS[TSname][i]-TSrecL8[i])**2 for i,v in enumerate(TSrecL8)])
+		TSsse[TSname].append(sse)
 
-	return TSwfv,TSsse
+		TSrec[TSname] = [TSrecL2,TSrecL4,TSrecL6,TSrecL8]
+
+		# Plotting
+		# fig = plt.figure()
+		# ax = fig.add_subplot(111)
+		# ax.plot(TS[TSname],label=TSname)
+		# ax.plot(TSrecL2,label=TSname+' (L2) SSE: '+str(TSsse[TSname][0]))
+		# ax.plot(TSrecL4,label=TSname+' (L4) SSE: '+str(TSsse[TSname][1]))
+		# ax.plot(TSrecL6,label=TSname+' (L6) SSE: '+str(TSsse[TSname][2]))
+		# ax.plot(TSrecL8,label=TSname+' (L8) SSE: '+str(TSsse[TSname][3]))
+		# plt.legend(prop={'size':6},loc=0)
+		# plt.savefig(TSname+'.pdf',edgecolor='b', format='pdf')
+
+	return TSwfv,TSsse,TSrec
